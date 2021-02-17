35b893f22754385e7a9e124b48da44d8a7535671
victory776 <elmerpalaciosweb@hotmail.com>
2021-02-17 14:32:47 -0500

    desktop layout of landing page

--- components/HomeContent/index.js
+++ components/HomeContent/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import CustomButton from "../Button";
+import { PROPS_LIST } from "../../enum";
 
 import "./style.scss";
 
@@ -17,6 +18,39 @@ const HomeContent = () => {
           to your own applications, SIP trunks and speech services.
         </h5>
         <CustomButton text="Sign up for free" />
+      </div>
+      <div className="home-page-history">
+        <div className="home-page-history-top">
+          <img
+            className="home-page-history-top-logo"
+            src="/images/img-logo-big.svg"
+            alt="big-logo"
+          />
+          <div className="home-page-history-top-phone flex justify-center items-center rounded-full bg-white">
+            <img src="/images/icon-phone.svg" alt="phone-icon" />
+          </div>
+        </div>
+        {PROPS_LIST.map((prop, index) => (
+          <div
+            key={index}
+            className={`history-item ${index === 0 ? "first" : ""} ${
+              index === PROPS_LIST.length - 1 ? "last" : ""
+            }`}
+          >
+            <h6 className="history-item-title text-third">{prop.title}</h6>
+            <p className="history-item-text text-fourth">{prop.text}</p>
+            <div className="history-item-icon">
+              <img src={prop.icon} alt="prop-icon" />
+            </div>
+          </div>
+        ))}
+        <div className="home-page-history-bottom">
+          <div className="home-page-history-bottom-left" />
+          <div className="home-page-history-bottom-right" />
+        </div>
+        <div className="flex justify-center">
+          <CustomButton text="Try it Free" />
+        </div>
       </div>
     </div>
   );
--- enum/index.js
+++ enum/index.js
@@ -1,3 +1,4 @@
 export { default as INTERNAL_LINKS } from "./InternalLinks";
 export { default as HEADER_MENULIST } from "./HeaderMenuList";
 export { default as FOOTER_MENULIST } from "./FooterMenuList";
+export { default as PROPS_LIST } from "./PropsList";
